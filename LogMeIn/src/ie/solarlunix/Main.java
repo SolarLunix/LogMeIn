b71dd6ab0e488f2c7aef8b14ec877756644ac224
SolarLunix <solarlunix@gmail.com>
2018-01-30 15:43:43 +0000

    Started LogMeIn

---
+++ LogMeIn/src/ie/solarlunix/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ie.solarlunix;
+
+/**
+ * @author Melissa Melaugh
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
